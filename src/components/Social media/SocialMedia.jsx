816eba54c31875059f110fd03aa6ba39184a8d77
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-01-01 00:31:37 +0530

    Added social media section

--- src/components/Social media/SocialMedia.jsx
+++ src/components/Social media/SocialMedia.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './socialMedia.css'
+
+import dropbox from '../../img/dropbox.png'
+import google from '../../img/google.png'
+import shopify from '../../img/shopify.png'
+import slack from '../../img/slack.png'
+
 function SocialMedia() {
   return (
-    <div>SocialMedia</div>
+    <>
+    <div className="socialMedia">
+      <img src={dropbox} alt="" />
+      <img src={shopify} alt="" />
+      <img src={google} alt="" />
+      <img src={slack} alt="" />
+    </div>
+    </>
   )
 }
 
