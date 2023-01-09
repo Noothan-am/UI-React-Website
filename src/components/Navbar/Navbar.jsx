1923165720e7f2b5af6da1ef4c4cb823d5d7e189
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-01-09 17:42:39 +0530

    added signin and sighnup to hamberger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React,{ useState }  from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import logo from '../../img/logo.png'
 import { BiAlignRight } from "react-icons/bi";
 const Navbar = () => {
     const [toggleMenu, settoggleMenu] = useState(false);
-    const clicked=()=>{
-        toggleMenu? settoggleMenu(false):settoggleMenu(true);
+    const clicked = () => {
+        toggleMenu ? settoggleMenu(false) : settoggleMenu(true);
     }
     return (
         <>
@@ -24,16 +24,20 @@ const Navbar = () => {
                     <button>Sign Up</button>
                 </div>
                 <div className="navbar_menu" onClick={clicked}>
-                <BiAlignRight/>
-                 {
-                    toggleMenu&&
-                    <div className="navbar_links-toggle">
-                        <p><a href="#openai">What is OpenAI?</a></p>
-                        <p><a href="#earlyAccess">Get early Access</a></p>
-                        <p><a href="#documentation">Documentation</a></p>
-                        <p><a href="#socialMedia">Connect Us</a></p>
-                    </div>
-                }
+                    <BiAlignRight />
+                    {
+                        toggleMenu &&
+                        <>
+                            <div className="navbar_links-toggle">
+                                <p><a href="#openai">What is OpenAI?</a></p>
+                                <p><a href="#earlyAccess">Get early Access</a></p>
+                                <p><a href="#documentation">Documentation</a></p>
+                                <p><a href="#socialMedia">Connect Us</a></p>
+                                <p>Sign In</p>
+                                <button>Sign Up</button>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </>
